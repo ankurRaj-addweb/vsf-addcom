3a3cb6952b0b28e74ee4e15a9a5202708a2e9dde
jainyashi <yashi.j.addweb@gmail.com>
2022-03-25 19:30:03 +0530

    AwOverlay

---
+++ stories/atoms/AwOverlay/AwOverlay.vue
@@ -0,0 +1,31 @@
+<template functional>
+  <transition :name="props.transition">
+    <div
+      v-if="props.visible"
+      ref="overlay"
+      :class="[data.class, data.staticClass, 'aw-overlay']"
+      @click="listeners.click && listeners.click($event)"
+    ></div>
+  </transition>
+</template>
+<script>
+export default {
+  name: "AwOverlay",
+  props: {
+    transition: {
+      type: String,
+      default: "aw-fade",
+    },
+    visible: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+<!-- <style lang="scss">
+
+@import "~@storefront-ui/shared/styles/components/atoms/SfOverlay.scss";
+
+
+</style> -->
