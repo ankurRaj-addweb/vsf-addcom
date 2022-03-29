cb21a43416299fa4ee99343be1922473d0980302
devarsh-addweb <devarsh.addweb@gmail.com>
2022-03-29 17:21:55 +0530

    alert.baner.image.searchbr

---
+++ stories/molecules/AwSearchBar/AwSearchBar.vue
@@ -0,0 +1,52 @@
+<template>
+  <AwInput
+    v-focus
+    class="aw-search-bar"
+    type="search"
+    name="search"
+    :value="value"
+    :placeholder="placeholder"
+    :icon="icon"
+    v-bind="$attrs"
+    v-on="listeners"
+    @keyup.enter="$emit('input', value)"
+  >
+  </AwInput>
+</template>
+<script>
+import AwInput from "../../atoms/AwInput/AwInput.vue";
+// import { focus } from "../../../utilities/directives";
+
+export default {
+  name: "AwSearchBar",
+  components: { AwInput },
+  directives: {
+    focus,
+  },
+  inheritAttrs: false,
+  props: {
+    placeholder: {
+      type: String,
+      default: "",
+    },
+    value: {
+      type: [Number, String],
+      default: null,
+    },
+    icon: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  computed: {
+    listeners() {
+      return {
+        ...this.$listeners,
+        "keyup.esc": () => this.$emit("input", ""),
+        blur: () => this.$emit("blur"),
+        "click:icon": () => this.$emit("click:icon", this.value),
+      };
+    },
+  },
+};
+</script>
