39ecb2272d52764ede646ab452ce4e349531ec6f
aditya <aditya.d.addweb@gmail.com>
2022-03-29 15:50:27 +0530

    AwCircle AwArrow

---
+++ stories/atoms/Awcirclelcon/AwCircleIcon.vue
@@ -0,0 +1,49 @@
+<template functional>
+  <component
+    :is="injections.components.AwButton"
+    :class="[data.class, data.staticClass, 'aw-circle-icon']"
+    v-bind="data.attrs"
+    :disabled="props.disabled"
+    v-on="listeners"
+  >
+    <slot>
+      <component
+        :is="injections.components.AwIcon"
+        aria-hidden="true"
+        class="aw-circle-icon__icon"
+        :icon="props.icon"
+        :size="props.iconSize"
+      />
+    </slot>
+  </component>
+</template>
+<script>
+import AwButton from "../AwButton/AwButton.vue";
+import AwIcon from "../AwIcon/AwIcon.vue";
+export default {
+  name: "AwCircleIcon",
+  inject: {
+    components: {
+      default: {
+        AwButton,
+        AwIcon,
+      },
+    },
+  },
+  props: {
+    icon: {
+      type: [String, Array],
+      default: "home",
+    },
+    iconSize: {
+      type: String,
+      default: "",
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
