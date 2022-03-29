30e6e076aade7701e7a00160f7a56c9667affae2
kajaladdweb <kajal.pa.addweb@gmail.com>
2022-03-29 17:31:41 +0530

    updated

---
+++ stories/molecules/AwProductOption/AwProductOption.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="aw-product-option">
+    <slot name="color" v-bind="{ color }">
+      <div
+        :class="{ 'display-none': !color }"
+        class="aw-product-option__color"
+        :style="{ background: color }"
+      ></div>
+    </slot>
+    <slot name="label" v-bind="{ label }">
+      <div class="aw-product-option__label">{{ label }}</div>
+    </slot>
+  </div>
+</template>
+<script>
+export default {
+  name: "AwProductOption",
+  props: {
+    label: {
+      type: String,
+      default: "",
+    },
+    color: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
+
