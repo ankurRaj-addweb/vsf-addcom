627d0f4f324507f088152c8c67e15f23b28dce77
avani9799 <avani.s.addweb@gmail.com>
2022-03-30 10:51:16 +0530

    AwSelect & AwDropdown & AwList

---
+++ stories/organisms/AwList/AwList.vue
@@ -0,0 +1,13 @@
+<template>
+  <ul class="aw-list">
+    <slot />
+  </ul>
+</template>
+<script>
+import AwListItem from "./_internal/AwListItem.vue";
+import Vue from "vue";
+Vue.component("AwListItem", AwListItem);
+export default {
+  name: "AwList",
+};
+</script>
