cb8082ef3c45f168e78ff34c880ab077f51ce8d8
guptanikhil11 <nikhil.addweb@gmail.com>
2022-03-30 12:26:36 +0530

    AwSkeleton, AwImage, AwColor

---
+++ stories/atoms/AwSkeleton/AwSkeleton.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="aw-skeleton" :class="classFromProps">
+    <slot />
+  </div>
+</template>
+<script>
+export default {
+  name: "AwSkeleton",
+  props: {
+    type: {
+      type: String,
+      default: "paragraph",
+      validator: (value) => ["paragraph", "image", "button", "input", "avatar"],
+    },
+  },
+  computed: {
+    classFromProps() {
+      return `aw-skeleton--${this.type}`;
+    },
+  },
+};
+</script>
