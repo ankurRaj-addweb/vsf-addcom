de677b2c25ef58a452a5cb8d984728fe3fb7a6dc
kajaladdweb <kajal.pa.addweb@gmail.com>
2022-04-01 13:34:05 +0530

    AwCallToAction AwModal AwCard

---
+++ stories/molecules/AwCard/AwCard.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="aw-card">
+    <AwImage
+      :width="imageWidth"
+      :height="imageHeight"
+      :src="image"
+      :alt="title"
+      class="aw-card__image"
+    />
+    <slot name="details" v-bind="{ title, description, titleLevel }">
+      <div class="aw-card__details">
+        <AwHeading
+          :title-level="titleLevel"
+          :title="title"
+          class="aw-card__title"
+        />
+        <p class="aw-card__description">
+          {{ description }}
+        </p>
+      </div>
+    </slot>
+    <slot name="action">
+      <AwButton :link="link" class="aw-card__action" v-on="$listeners">
+        {{ buttonText }}
+      </AwButton>
+    </slot>
+  </div>
+</template>
+<script>
+import AwButton from "../../atoms/AwButton/AwButton.vue";
+import AwHeading from "../../atoms/AwHeadings/AwHeading.vue";
+import AwImage from "../../atoms/AwImage/AwImage.vue";
+
+export default {
+  name: "AwCard",
+  components: {
+    AwButton,
+    AwHeading,
+    AwImage,
+  },
+  props: {
+    image: {
+      type: String,
+      default: "",
+    },
+    imageWidth: {
+      type: [Number, String],
+      required: true,
+    },
+    imageHeight: {
+      type: [Number, String],
+      required: true,
+    },
+    title: {
+      type: String,
+      default: "",
+    },
+    titleLevel: {
+      type: Number,
+      default: 3,
+    },
+    description: {
+      type: String,
+      default: "",
+    },
+    link: {
+      type: String,
+      default: null,
+    },
+    buttonText: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
---
+++ stories/molecules/AwModal/AwModal.vue
@@ -0,0 +1,146 @@
+<template>
+  <section class="aw-modal" :class="[staticClass, className]">
+    <AwOverlay
+      v-if="overlay"
+      class="aw-modal__overlay"
+      :transition="transitionOverlay"
+      :visible="visible"
+    >
+    </AwOverlay>
+    <transition :name="transitionModal">
+      <div
+        v-if="visible"
+        v-focus-trap
+        v-click-outside="checkPersistence"
+        class="aw-modal__container"
+      >
+        <slot name="modal-bar">
+          <AwBar
+            class="aw-modal__bar smartphone-only"
+            :close="cross"
+            :title="title"
+            @click:close="close"
+          />
+        </slot>
+        <AwButton
+          :class="{ 'display-none': !cross }"
+          class="aw-button--pure aw-modal__close desktop-only"
+          aria-label="Close modal"
+          type="button"
+          data-testid="close-button"
+          @click="close"
+        >
+          <slot name="close">
+            <AwIcon icon="cross" size="0.875rem" color="gray-secondary" />
+          </slot>
+        </AwButton>
+        <div ref="content" class="aw-modal__content">
+          <slot />
+        </div>
+      </div>
+    </transition>
+  </section>
+</template>
+<script>
+import AwBar from "../../molecules/AwBar/AwBar.vue";
+import AwOverlay from "../../atoms/AwOverlay/AwOverlay.vue";
+import AwIcon from "../../atoms/AwIcon/AwIcon.vue";
+import AwButton from "../../atoms/AwButton/AwButton.vue";
+import  {disableBodyScroll, clearAllBodyScrollLocks } from "body-scroll-lock";
+import  focusTrap  from "../../utilities/directives";
+import  clickOutside  from "../../utilities/directives";
+import  isClient  from "../../utilities/helpers";
+export default {
+  name: "AwModal",
+  directives: { focusTrap, clickOutside },
+  components: {
+    AwBar,
+    AwOverlay,
+    AwIcon,
+    AwButton,
+  },
+  model: {
+    prop: "visible",
+    event: "close",
+  },
+  props: {
+    title: {
+      type: String,
+      default: "",
+    },
+    visible: {
+      type: Boolean,
+      default: false,
+    },
+    cross: {
+      type: Boolean,
+      default: true,
+    },
+    overlay: {
+      type: Boolean,
+      default: true,
+    },
+    persistent: {
+      type: Boolean,
+      default: false,
+    },
+    transitionOverlay: {
+      type: String,
+      default: "aw-fade",
+    },
+    transitionModal: {
+      type: String,
+      default: "aw-fade",
+    },
+  },
+  data() {
+    return {
+      staticClass: null,
+      className: null,
+    };
+  },
+  watch: {
+    visible: {
+      handler: function (value) {
+        if (!isClient) return;
+        if (value) {
+          this.$nextTick(() => {
+            disableBodyScroll(this.$refs.content);
+          });
+          document.addEventListener("keydown", this.keydownHandler);
+        } else {
+          clearAllBodyScrollLocks();
+          document.removeEventListener("keydown", this.keydownHandler);
+        }
+      },
+      immediate: true,
+    },
+  },
+  beforeDestroy() {
+    clearAllBodyScrollLocks();
+  },
+  methods: {
+    close() {
+      this.$emit("close", false);
+    },
+    checkPersistence() {
+      if (!this.persistent) {
+        this.close();
+      }
+    },
+    keydownHandler(e) {
+      if (e.key === "Escape" || e.key === "Esc" || e.keyCode === 27) {
+        this.close();
+      }
+    },
+    classHandler() {
+      if (this.staticClass !== this.$vnode.data.staticClass) {
+        this.staticClass = this.$vnode.data.staticClass;
+      }
+      if (this.className !== this.$vnode.data.class) {
+        this.className = this.$vnode.data.class;
+      }
+    },
+  },
+};
+</script>
